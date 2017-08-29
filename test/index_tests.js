be81d764ae64514508d64531dad2393d49110648
lee brian <leesinshingbrian@gmail.com>
2017-08-29 11:07:28 +0800

    before travis

--- test/index_tests.js
+++ test/index_tests.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const expect = require('chai').expect
 const app = require('../index')
+var assert = require('assert')
 
 describe('GET /', function () {
   describe('/', function () {
@@ -22,10 +23,34 @@ describe('GET /', function () {
 
 describe('CRUD taco test', function () {
   describe('GET/tacos', function () {
-    it('should response 200', function (done) {
+    it('should return all tacos', function (done) {
       request(app)
       .get('/tacos')
-      .expect(200, done)
+      .end(function (err, response) {
+        // console.log(response.status)
+        expect(response.status).to.equal(200)
+        // assert.strictEqual(200, response.status) same as above
+        expect(response.body).to.be.an('array')
+        // console.log(response.body[0])
+        if (response.body.length) {
+          expect(response.body[0]).to.have.property('amount')
+        }
+        done()
+      })
+    })
+  })
+
+  describe('POST/tacos', function () {
+    it('should create new taco', function (done) {
+      request(app)
+      .post('/tacos')
+      .send({
+        name: 'apa ini',
+        amount: 1000
+      })
+      .end(function (err, response) {
+        done()
+      })
     })
   })
 })
